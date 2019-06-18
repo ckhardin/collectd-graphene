9d64a7e22a0d32f2c7de27d8b8069a13676e64a2
Charles Hardin <ckhardin@gmail.com>
2019-06-17 22:33:51 -0700

    frontend: update to the later versions of graphql and the react scripts
    Update some of the package versions to address some warnings during
    the install and other build procedures. These changes should not be
    seen externally to any of the app changes.
    
    Signed-off-by: Charles Hardin <ckhardin@gmail.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -26,7 +26,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">GraphQL and D3 for RRD</h1>
+          <h1 className="App-title">GraphQL and React for RRD</h1>
         </header>
         <QueryRenderer
           environment={environment}
