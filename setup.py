d742bc8a40a2510777d7a36fbf3a0551d2b530ac
Charles Hardin <ckhardin@gmail.com>
2019-06-24 06:15:08 +0000

    Bump version: 0.2.0 → 0.3.0

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ requirements=[
 
 setup(
     name='collectd-graphene',
-    version='0.2.0',
+    version='0.3.0',
     author='Charles Hardin',
     author_email='ckhardin@gmail.com',
     license='MIT',
